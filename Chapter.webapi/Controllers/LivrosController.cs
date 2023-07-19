3b13f74874d7268b6504c45308ba03f08da7a407
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-07-19 17:22:11 -0300

    Demais pastas

--- Chapter.webapi/Controllers/LivrosController.cs
+++ Chapter.webapi/Controllers/LivrosController.cs
@@ -1,15 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using Chapter.WebApi.Models;
+using Chapter.WebApi.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
-namespace Chapter.webapi.Controllers
-{
+namespace Chapter.WebApi.Controllers{
     [Route("api/[controller]")]
     [ApiController]
-    public class LivrosController : ControllerBase
-    {
+
+    public class LivrosController : ControllerBase{
+        private readonly LivroRepository _livroRepository;
+        public LivrosController (LivroRepository livroRepository){
+            _livroRepository = livroRepository;
+        }
+        [HttpGet]
+        public IActionResult Listar(){
+            return Ok (_livroRepository.Listar());
+        }
+        }
     }
-}
+
--- Chapter.webapi/Program.cs
+++ Chapter.webapi/Program.cs
@@ -1,25 +1,17 @@
+using Chapter.WebApi.Contexts;
+using Chapter.WebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<LivroRepository, LivroRepository>();
 
 var app = builder.Build();
+app.UseRouting();
+app.UseEndpoints(endpoints =>{
+    endpoints.MapControllers();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+});
 
 app.Run();
